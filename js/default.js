06675817bb64d7226d1426d9fea0135f2a0eb16e
Unknown <g1627039@st.t-kougei.ac.jp>
2018-11-17 16:07:54 +0900

    2018_11_17
    other.htmlの作成,カウントダウンコマンド追加

--- js/default.js
+++ js/default.js
@@ -1,19 +1,43 @@
-var separate_time = function(time){
-  var sec   = time.getSeconds();
-  var min   = time.getMinutes();
-  var hours = time.getHours();
-  var days  = time.getDate();
-  var month = time.getMonth();
-  var year  = time.getFullYear();
-  return [sec, min, hours, days, month, year];
-;
+function getFileName(){
+		return window.location.href,split('/').pop();
+}
 
-var now = new Date();
-var counter = separate_time(now);
-document.getElementById('countdown').textContent = 
-  counter[5] + '年' + 
-  counter[4] + '月' + 
-  counter[3] + '日' + 
-  counter[2] + '時' + 
-  counter[1] + '分' +
-  counter[0] + '秒';
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+	opt = document.querySelector('option[value="other.html"]');
+}else{
+	opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+docment.getElementById('form').select.onchange = function(){
+	location.href = document.getElementById('form').select.value;
+}
+
+var separate_time = function(time){
+	var sec   = Math.floor((time / 1000) % 60);
+	var min   = Math.floor((time / 1000 / 60) % 60);
+	var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+	var days  = Math.floor( time / 1000 / 60 / 60 / 24);
+	return [sec, min, hours, days];
+}
+
+var update = function(){
+	var now = new Date();
+	var target = new Date(2020,7,24,0,0,0,0);
+	var diff = target.getTime() - now.getTime();
+	var counter = separate_time(diff);
+	document.getElementById('countdown').textContent = 
+		'東京オリンピックまであと ' +
+		counter[3] + '日' + 
+		counter[2] + '時' + 
+		counter[1] + '分' +
+		counter[0] + '秒';
+	refresh();
+}
+
+var refresh= function(){
+	setTimeout(update, 1000);
+}
+update();
