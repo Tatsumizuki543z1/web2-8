3220c7511946107894086dd799d063a5b4a6ed82
Tatsumizuki543z1 <38421209+Tatsumizuki543z1@users.noreply.github.com>
2018-11-19 14:45:56 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,47 @@
+function getFileName(){
+		return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+	opt = document.querySelector('option[value="other.html"]');
+}else{
+	opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function(){
+	location.href = document.getElementById('form').select.value;
+}
+
+var separate_time = function(time){
+	var sec   = Math.floor((time / 1000) % 60);
+	var min   = Math.floor((time / 1000 / 60) % 60);
+	var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+	var days  = Math.floor( time / 1000 / 60 / 60 / 24);
+	return [sec, min, hours, days];
+}
+
+var update = function(){
+	var now = new Date();
+	var target = new Date(2020,7,24,0,0,0,0);
+	var diff = target.getTime() - now.getTime();
+	var counter = separate_time(diff);
+	document.getElementById('countdown').textContent = 
+		'東京オリンピックまであと ' +
+		counter[3] + '日' + 
+		counter[2] + '時' + 
+		counter[1] + '分' +
+		counter[0] + '秒';
+	refresh();
+}
+
+var refresh= function(){
+	setTimeout(update, 1000);
+}
+update();
+
 //クッキー
 function setCookie(c_name,value,expiredays) {
 	// 有効期限の日付
@@ -53,47 +97,3 @@ setCookie('lastDate', current_time.toString(), 7);
 document.getElementById('remove_cookie').onsubmit = function(){
 	setCookie('lastDate', "", 0);
 }
-
-function getFileName(){
-		return window.location.href.split('/').pop();
-}
-
-var filename = getFileName();
-var opt;
-if(filename === 'other.html'){
-	opt = document.querySelector('option[value="other.html"]');
-}else{
-	opt = document.querySelector('option[value="index.html"]');
-}
-opt.selected = true;
-
-document.getElementById('form').select.onchange = function(){
-	location.href = document.getElementById('form').select.value;
-}
-
-var separate_time = function(time){
-	var sec   = Math.floor((time / 1000) % 60);
-	var min   = Math.floor((time / 1000 / 60) % 60);
-	var hours = Math.floor((time / 1000 / 60 / 60) % 24);
-	var days  = Math.floor( time / 1000 / 60 / 60 / 24);
-	return [sec, min, hours, days];
-}
-
-var update = function(){
-	var now = new Date();
-	var target = new Date(2020,7,24,0,0,0,0);
-	var diff = target.getTime() - now.getTime();
-	var counter = separate_time(diff);
-	document.getElementById('countdown').textContent = 
-		'東京オリンピックまであと ' +
-		counter[3] + '日' + 
-		counter[2] + '時' + 
-		counter[1] + '分' +
-		counter[0] + '秒';
-	refresh();
-}
-
-var refresh= function(){
-	setTimeout(update, 1000);
-}
-update();
