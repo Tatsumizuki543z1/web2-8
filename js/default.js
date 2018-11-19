c15cfaf550b4f3dba45e99240515882d4fecceb4
Tatsumizuki543z1 <38421209+Tatsumizuki543z1@users.noreply.github.com>
2018-11-19 17:29:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,99 +1,93 @@
 function getFileName(){
-		return window.location.href.split('/').pop();
-}
+    return window.location.href.split('/').pop();
+  }
 
-var filename = getFileName();
-var opt;
-if(filename === 'other.html'){
-	opt = document.querySelector('option[value="other.html"]');
-}else{
-	opt = document.querySelector('option[value="index.html"]');
-}
-opt.selected = true;
-
+  var filename = getFileName();
+  var opt;
+  if(filename === 'other.html'){
+    opt = document.querySelector('option[value="other.html"]');
+  }else{
+    opt = document.querySelector('option[value="index.html"]');
+  }
+  opt.selected = true;
+	
 document.getElementById('form').select.onchange = function(){
-	location.href = document.getElementById('form').select.value;
-}
+    location.href = document.getElementById('form').select.value;
+  }
 
 var separate_time = function(time){
-	var sec   = Math.floor((time / 1000) % 60);
-	var min   = Math.floor((time / 1000 / 60) % 60);
-	var hours = Math.floor((time / 1000 / 60 / 60) % 24);
-	var days  = Math.floor( time / 1000 / 60 / 60 / 24);
-	return [sec, min, hours, days];
+  var sec = time.getSeconds();
+  var min = time.getMinutes();
+  var hours = time.getHours();
+  var days = time.getDate();
+  var month = time.getMonth();
+  var year = time.getFullYear();
+  return[sec, min, hours, days, month, year];
 }
 
 var update = function(){
-	var now = new Date();
-	var target = new Date(2020,7,24,0,0,0,0);
-	var diff = target.getTime() - now.getTime();
-	var counter = separate_time(diff);
-	document.getElementById('countdown').textContent = 
-		'東京オリンピックまであと ' +
-		counter[3] + '日' + 
-		counter[2] + '時' + 
-		counter[1] + '分' +
-		counter[0] + '秒';
-	refresh();
+var now = new Date();
+var counter = separate_time(now); 
+document.getElementById('countdown').textContent = '現在' + counter[5] + '年' +counter[4] + '月' +counter[3] + '日' +counter[2] + '時' +counter[1] + '分' +counter[0] + '秒' ;
+refresh();
 }
 
-var refresh= function(){
-	setTimeout(update, 1000);
+var refresh = function(){
+  setTimeout(update, 1000);
 }
+
 update();
 
-//クッキー
-function setCookie(c_name,value,expiredays) {
-	// 有効期限の日付
-	var extime = new Date().getTime();
-	var citime = new Date(extime + (60*60*24*1000*expiredays));
-	var exdate = citime.toUTCString();
+function setCookie(c_name,value,expiredays){
+  var extime = new Date().getTime();
+  var cltime = new Date(extime + (60*60*24*1000*expiredays));
+  var exdate = cltime.toUTCString();
 
-	var s="";
-	s += c_name + "=" + escape(value);
-	s += "; path=" + location.pathname;
-	if(expiredays){
-		s += "; expires=" + exdate + "; ";
-	}else{
-		s += "; ";
-	}
-	//クッキーに保存
-	document.cookie = s;
+  var s="";
+  s += c_name +"="+ escape(value);
+  s += "; path="+ location.pathname;
+  if(expiredays){
+    s += "; expires=" +exdate+"; ";
+  }else{
+    s += "; ";
+  }
+
+  document.cookie=s;
 }
 
-//クッキーの値を取得
 function getCookie(c_name){
-	var st = "";
-	var ed = "";
-	if(0 < document.cookie.length){
-		//クッキーの値を取り出す
-		st = document.cookie.indexOf(c_name + "=");
-		if(st! = -1){
-			st = st + c_name.length + 1;
-			ed = document.cookie.indexOf(";", st);
-			if(ed == 1)
-				ed = document.cookie.length;
-
-			//値をデコード
-			return unescape(document.cookie.substring(st, ed));
-		}
-	}
-	return "";
+  var st="";
+  var ed="";
+  if(0 < document.cookie.length){
+    st=document.cookie.indexOf(c_name + "=");
+    if(st!=-1){
+      st=st+c_name.length+1;
+      ed=document.cookie.indexOf(";",st);
+      if(ed==-1) ed=document.cookie.length;
+      return unescape(document.cookie.substring(st,ed));
+    }
+  }
+  return "";
 }
 
-//Cookieを読み込み、表示する文章を変える
 var last_date = getCookie('lastDate');
 if(last_date){
-	document.getElementById('cookie').textContent = '前回訪れた時間:' + last_date;
+  document.getElementById('cookie').textContent = '前回訪れた時間:' + last_date;
 }else{
-	document.getElementById('cookie').textContent = 'はじめまして';
+  document.getElementById('cookie').textContent = 'はじめまして';
 }
-	
-//新しい値の保存
+
 var current_time = new Date();
 setCookie('lastDate', current_time.toString(), 7);
 
-//クッキーの削除
 document.getElementById('remove_cookie').onsubmit = function(){
-	setCookie('lastDate', "", 0);
+  setCookie('lastDate', "", 0);
+};
+
+var thmubs = document.querySelectorAll('.thumb');
+for(idx in thmubs){
+  thmubs[idx].onclick = function(){
+    document.getElementById("bigimg").src = 'img/' + this.dataset.image + '.jpg';
+  }
 }
+
